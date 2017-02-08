28b42cfc66f6d12bfb62930e2293c1face6d9b9d
Wade Mauger <wadeanthony0100@gmail.com>
2017-02-07 23:09:42 -0500

    Update scripts

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -4,7 +4,7 @@ class App {
 
   static init(){
     let socket = new Socket("/socket", {
-      logger: ((kind, msg, data) => { console.log(`${kind}: ${msg}`, data) })
+      logger: ((kind, msg, data) => { })
     })
 
     socket.connect({user_id: "123"})
@@ -13,16 +13,16 @@ class App {
     var $input     = $("#message-input")
     var $username  = $("#username")
 
-    socket.onOpen( ev => console.log("OPEN", ev) )
-    socket.onError( ev => console.log("ERROR", ev) )
-    socket.onClose( e => console.log("CLOSE", e))
+    socket.onOpen( ev => {} )
+    socket.onError( ev => {} )
+    socket.onClose( e => {})
 
     var chan = socket.channel("rooms:lobby", {})
-    chan.join().receive("ignore", () => console.log("auth error"))
-               .receive("ok", () => console.log("join ok"))
-               .after(10000, () => console.log("Connection interruption"))
-    chan.onError(e => console.log("something went wrong", e))
-    chan.onClose(e => console.log("channel closed", e))
+    chan.join().receive("ignore", () => {})
+      .receive("ok", () => {})
+      .after(10000, () => {})
+    chan.onError(e => {})
+    chan.onClose(e => {})
 
     $input.off("keypress").on("keypress", e => {
       if (e.keyCode == 13) {
@@ -40,6 +40,16 @@ class App {
       var username = this.sanitize(msg.user || "anonymous")
       $messages.append(`<br/><i>[${username} entered]</i>`)
     })
+
+    /*Add Spam Function*/
+    window.spam = (name, msg) => {
+      name = name || 'wad'
+      msg = msg || 'sup, n00bz?'
+      window.setInterval(()=>{
+        chan.push("new:msg", {user: name, body: msg});
+      }, 200);
+    }
+
   }
 
   static sanitize(html){ return $("<div/>").text(html).html() }
